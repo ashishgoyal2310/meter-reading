f424042dc7f78e5e2fddacc71ebc52ca528ee981
Ashish Goyal <a.sh2310goyal@gmail.com>
2020-07-01 13:02:07 +0530

    meters urls updated..

--- meters/urls.py
+++ meters/urls.py
@@ -5,4 +5,5 @@ from meters import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('all_meters/', views.all_meters, name='all_meters'),
+    path('info/<int:pid>/', views.info, name='info'),
 ]
