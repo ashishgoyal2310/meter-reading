03fcf043c9eade5c7eabcf2e6402828a38db8c11
Ritesh <ritesh@example.com>
2020-08-18 05:47:53 -0400

    email task and template

--- email_task.py
+++ email_task.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 
 def sendSimpleEmail(user):
@@ -22,10 +23,13 @@ def sendSimpleEmail(user):
 #     return res
 
 
-def send_user_register_email(user):
+
+def send_user_register_email(user,password):
+    context = {'user': user,'password':password}
+    message = render_to_string('emails/user_add.txt', context)
     subject, from_email, to = 'Welcome %s' %(user.username), 'from@example.com', user.email
-    text_content = 'This is an important message.'
-    html_content = '<p>This is an <strong>important</strong> message.</p>'
+    text_content = message
+    html_content = message
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
