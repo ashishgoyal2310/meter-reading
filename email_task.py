ac76912dae70ef8effda1bb441d15f707a8b3046
Ashish Goyal <ashish2.goyal@orange.com>
2020-08-20 15:46:33 +0530

    Update for email task for password forgot and reset

--- email_task.py
+++ email_task.py
@@ -25,14 +25,28 @@ def send_user_register_email(user, password):
     msg.attach_alternative(html_content, "text/html")
     msg.send()
 
-# def send_forgot_password_email(user,token):
-#     from_email, to = 'from@example.com', user.email
 
-#     context = {'user': user,'password':password}
-#     subject = render_to_string('emails/user_add_subject.txt', context)
-#     text_content = render_to_string('emails/user_add.txt', context)
-#     html_content = text_content
+def send_forgot_password_email(user, token):
+    from_email, to = 'from@example.com', user.email
 
-#     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
-#     msg.attach_alternative(html_content, "text/html")
-#     msg.send()
+    context = {'user': user,'token': token}
+    subject = render_to_string('emails/user_forgot_password_subject.txt', context)
+    text_content = render_to_string('emails/user_forgot_password.txt', context)
+    html_content = text_content
+
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+
+
+def send_reset_password_success_email(user):
+    from_email, to = 'from@example.com', user.email
+
+    context = {'user': user}
+    subject = render_to_string('emails/user_reset_password_subject.txt', context)
+    text_content = render_to_string('emails/user_reset_password.txt', context)
+    html_content = text_content
+
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
